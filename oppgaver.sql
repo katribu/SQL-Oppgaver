238ebbc9c3f33f4d0c6ca320e873c30cfd10a570
katb <katrinaburwash_17@hotmail.com>
2023-01-12 14:46:15 +0100

    First question commit

--- oppgaver.sql
+++ oppgaver.sql
@@ -1,4 +1,6 @@
 --Get the number of cities with a population over the average population.
+
+-- ==================== EXAMPLES ================ --
 SELECT COUNT(*)
 FROM city
 WHERE Population >=
@@ -12,3 +14,18 @@ From city where countrycode IN
 -- Compare by percentage the sizes of the countries in relation the largest country (i.e. surface area)
 SELECT Name, SurfaceArea /(SELECT MAX(surfacearea) FROM country) * 100
 FROM countrycode ORDER BY surfacearea DESC;
+
+-- ========== SUBQUERIES OPPGAVER =========== --
+--a)
+-- JOIN EX --
+SELECT country.name
+FROM country
+LEFT JOIN city ON country.code = city.countrycode
+WHERE city.name IS NULL;
+
+-- SUBQUERY EX --
+SELECT name as countryname
+FROM country where code NOT IN (
+	SELECT DISTINCT countrycode
+	from city
+);
